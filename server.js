d8e578e8e4337973884c447f5efccb9b838bf39e
Hobbes <43449480+ignaciuk@users.noreply.github.com>
2019-05-19 09:50:16 -0400

    created folder structure

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = reauire("express");
+var mongoose = require("mongoose");
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+var db = require("./models");
+
+var PORT = 3000;
+
+var app = express();
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+app.use(express.)
